c34f4fde11921f8a1b4bc6182964cc01344d156a
caiodsc <caio.dscamara@gmail.com>
2024-12-27 13:26:36 -0400

    chore: bundle gem simple_lock

---
+++ lib/simple_lock.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "simple_lock/version"
+
+module SimpleLock
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/simple_lock_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SimpleLock do
+  it "has a version number" do
+    expect(SimpleLock::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
